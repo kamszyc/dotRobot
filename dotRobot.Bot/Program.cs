4353a3634336f76c371bb56f5fba20df3e54cff6
Kamil Szyc <kamszyc@gmail.com>
2025-05-15 23:43:42 +0200

    Add simple BLE implementation to Bot project

--- dotRobot.Bot/Program.cs
+++ dotRobot.Bot/Program.cs
@@ -1,20 +1,73 @@
+using dotRobot.Common;
+using nanoFramework.Device.Bluetooth;
+using nanoFramework.Device.Bluetooth.GenericAttributeProfile;
 using System;
 using System.Diagnostics;
 using System.Threading;
 
-namespace dotRobot.Bot
+namespace dotRobot
 {
     public class Program
     {
         public static void Main()
         {
-            Debug.WriteLine("Hello from nanoFramework!");
+            BluetoothLEServer server = BluetoothLEServer.Instance;
+            server.DeviceName = "dotRobot";
+
+            GattServiceProviderResult result = GattServiceProvider.Create(Guids.ServiceGuid);
+            if (result.Error != BluetoothError.Success)
+            {
+                return;
+            }
+
+            GattServiceProvider serviceProvider = result.ServiceProvider;
+            GattLocalService service = serviceProvider.Service;
+
+            var characteristicResult = service.CreateCharacteristic(Guids.RobotControlCharactericticGuid,
+                new GattLocalCharacteristicParameters()
+                {
+                    CharacteristicProperties = GattCharacteristicProperties.Write,
+                    UserDescription = "Robot control",
+                });
+
+            if (characteristicResult.Error != BluetoothError.Success)
+            {
+                return;
+            }
+
+            var robotControlCharacteristic = characteristicResult.Characteristic;
+            robotControlCharacteristic.WriteRequested += RobotControlCharacteristic_WriteRequested;
+
+
+            serviceProvider.StartAdvertising(new GattServiceProviderAdvertisingParameters()
+            {
+                IsConnectable = true,
+                IsDiscoverable = true
+            });
 
             Thread.Sleep(Timeout.Infinite);
+        }
 
-            // Browse our samples repository: https://github.com/nanoframework/samples
-            // Check our documentation online: https://docs.nanoframework.net/
-            // Join our lively Discord community: https://discord.gg/gCyBu8T
+        private static void RobotControlCharacteristic_WriteRequested(GattLocalCharacteristic sender, GattWriteRequestedEventArgs writeRequestEventArgs)
+        {
+            GattWriteRequest request = writeRequestEventArgs.GetRequest();
+
+            if (request.Value.Length != 5)
+            {
+                request.RespondWithProtocolError((byte)BluetoothError.NotSupported);
+                return;
+            }
+
+            DataReader rdr = DataReader.FromBuffer(request.Value);
+            string command = rdr.ReadString(5);
+
+            // Respond if Write requires response
+            if (request.Option == GattWriteOption.WriteWithResponse)
+            {
+                request.Respond();
+            }
+
+            Debug.WriteLine($"Received command={command}");
         }
     }
 }
